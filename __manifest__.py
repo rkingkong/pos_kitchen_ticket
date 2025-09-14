5e24d0f3364c60d2396b60f707b0940c49b8a04d
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-14 13:10:21 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "POS Kitchen Ticket",
+    "summary": "Comanda de cocina en formato ticket para POS",
+    "version": "17.0.1.0",
+    "license": "LGPL-3",
+    "author": "Kesiyos",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/pos_kitchen_report.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
