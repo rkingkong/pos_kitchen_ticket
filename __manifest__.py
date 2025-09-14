0f22ac12d6acb5ccc204fe9c52f7c3e4b692c013
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-14 13:14:47 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,18 @@
 {
     "name": "POS Kitchen Ticket",
     "summary": "Comanda de cocina en formato ticket para POS",
-    "version": "17.0.1.0",
+    "version": "17.0.2.0",
     "license": "LGPL-3",
     "author": "Kesiyos",
     "depends": ["point_of_sale"],
     "data": [
-        "views/pos_kitchen_report.xml",
+        "views/pos_kitchen_report.xml",  # el reporte backend (opcional)
     ],
-    "application": False,
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_kitchen_ticket/static/src/js/kitchen_button.js",
+            "pos_kitchen_ticket/static/src/xml/kitchen_templates.xml",
+        ],
+    },
     "installable": True,
 }
