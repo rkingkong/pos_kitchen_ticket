cbaaf9030d81df3cba2799bd4157b229ddc2d382
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-14 18:22:33 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,20 @@
 {
     "name": "POS Kitchen Ticket",
-    "version": "17.0.3.0",
+    "version": "17.0.4.0",
     "license": "LGPL-3",
     "author": "Kesiyos",
     "depends": ["point_of_sale"],
     "data": [
-        "views/pos_kitchen_report.xml",   # <- backend PDF action + template
+        "views/pos_kitchen_report.xml",
     ],
     "assets": {
-        "point_of_sale.assets": [
-            "pos_kitchen_ticket/static/src/js/boot_log.js",
-            "pos_kitchen_ticket/static/src/js/kitchen_button.js",
-            "pos_kitchen_ticket/static/src/xml/kitchen_templates.xml",
-        ],
-        "web.assets_qweb": [
-            "pos_kitchen_ticket/static/src/xml/kitchen_templates.xml",
+        "point_of_sale._assets_pos": [
+            "pos_kitchen_ticket/static/src/js/**/*.js",
+            "pos_kitchen_ticket/static/src/xml/**/*.xml",
+            "pos_kitchen_ticket/static/src/css/**/*.css",
         ],
     },
     "installable": True,
+    "auto_install": False,
+    "application": False,
 }
