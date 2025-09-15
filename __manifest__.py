a1763d728ab78e86ef139c779cfc48c594620cbd
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-14 22:06:18 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,32 +1,14 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'POS Kitchen Ticket',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.0',
     'category': 'Point of Sale',
-    'summary': 'Print kitchen tickets from POS payment screen',
-    'description': """
-        Kitchen Ticket Printing for Restaurants
-        ========================================
-        - Adds kitchen ticket button to payment screen
-        - Prints order details for kitchen
-        - Support for table and waiter information
-        - Works with Odoo 17 Community
-    """,
-    'author': 'Kesiyos Restaurant',
-    'website': 'https://kesiyos.gt',
+    'summary': 'Kitchen ticket button in payment screen',
     'depends': ['point_of_sale'],
-    'data': [
-        'views/pos_assets.xml',
-    ],
+    'installable': True,
     'assets': {
-        'point_of_sale._assets_pos': [
+        'point_of_sale.assets': [
             'pos_kitchen_ticket/static/src/js/PaymentScreenButton.js',
             'pos_kitchen_ticket/static/src/xml/PaymentScreenButton.xml',
-            'pos_kitchen_ticket/static/src/xml/KitchenReceipt.xml',
-            'pos_kitchen_ticket/static/src/css/kitchen_ticket.css',
         ],
     },
-    'installable': True,
-    'application': False,
-    'license': 'LGPL-3',
 }
