74557caa77372927a12f6bcaaa7b86f83ca404f5
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-21 18:55:28 -0600

    Create pos_session.py

---
+++ models/pos_session.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+    
+    def _loader_params_pos_order(self):
+        """Agregar campos de cocina a los parámetros de carga"""
+        result = super()._loader_params_pos_order()
+        result['search_params']['fields'].extend([
+            'kitchen_printed', 
+            'kitchen_print_count',
+            'order_type',
+            'special_instructions'
+        ])
+        return result
+    
+    def _loader_params_pos_order_line(self):
+        """Agregar campos de cocina a las líneas de orden"""
+        result = super()._loader_params_pos_order_line()
+        result['search_params']['fields'].extend([
+            'kitchen_state',
+            'kitchen_notes',
+            'customer_note'
+        ])
+        return result
